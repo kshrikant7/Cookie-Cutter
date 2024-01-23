2fdd009e096da48e4f60bbaf1b7d5d358fdbe12a
kshrikant7 <1999kshrikant@gmail.com>
2024-01-23 18:26:50 +0530

    Refactor prediction function to use model.predict with n_periods=10

--- {{cookiecutter.project_slug}}/prediction/predict.py
+++ {{cookiecutter.project_slug}}/prediction/predict.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-# Load the model from disk'
+# Load the model from disk
 
 def make_prediction(model, X_test):
-    user_input = input("Please enter your input for prediction: ")
+    # user_input = input("Please enter your input for prediction: ")
     # Convert the user input to the format expected by your model
     # This is just an example, you'll need to adjust this to match your actual model
-    user_input = np.array([float(user_input)]).reshape(1, -1)
+    # user_input = np.array([float(user_input)]).reshape(1, -1)
+    # exog_future = None  # Define the variable exog_future
     # Make a prediction based on the user input
-    prediction = model.predict(user_input)
-    print(f"Prediction made for input {user_input}: {prediction}")
+    prediction = model.predict(n_periods=10)
+    print(f"Prediction: {prediction}")
+    return prediction
